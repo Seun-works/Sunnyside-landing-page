27be0db43468e49fecad0ad86f9f1ef041210720
SeunOgundipe <Sogundipe@gmail.com>
2021-10-04 15:50:45 +0100

    added adjustments for screen sizes

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ let menuOpen = false;
 function changeMenu() {
     if(!menuOpen) {
         heading.classList.add ('is-open');
-        mobileBtn.setAttribute("src", "../../images/icon-hamburger.svg");
+        mobileBtn.setAttribute("src", "images/icon-hamburger.svg");
         menuOpen = true;
     } else {
         heading.classList.remove ('is-open');
