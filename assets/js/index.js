6b0432cf0d9046ef828a3cefc82746448b231313
SeunOgundipe <Sogundipe@gmail.com>
2021-10-04 08:53:02 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const heading = document.getElementById('head');
+const mobileBtn = document.querySelector('.mobile-button');
+
+
+let menuOpen = false;
+
+function changeMenu() {
+    if(!menuOpen) {
+        heading.classList.add ('is-open');
+        mobileBtn.setAttribute("src", "../../images/icon-hamburger.svg");
+        menuOpen = true;
+    } else {
+        heading.classList.remove ('is-open');
+        mobileBtn.setAttribute("src", "images/icon-hamburger.svg");
+        menuOpen = false;
+    }
+}
